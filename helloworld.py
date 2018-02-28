075aa1a32440a477211d3ba2f53d9853fa5cdf07
Tim Oost <timoost@Air-van-Tim.fritz.box>
2018-02-28 20:36:38 +0100

    draw primitives added

--- helloworld.py
+++ helloworld.py
@@ -3,6 +3,7 @@ from pygame.locals import *
 
 pygame.init()
 DISPLAYSURF = pygame.display.set_mode((400,300))
+
 pygame.display.set_caption('Hello World!')
 while True:
     for event in pygame.event.get():
